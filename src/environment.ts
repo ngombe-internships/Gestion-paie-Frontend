a5c982396985ac55984eccfa3268c4c2047c8a07
Ulrichjack <jackeboyeur244@gmail.com>
2025-07-14 17:07:16 +0100

    mise a jour dynamique

--- src/environment.ts
+++ src/environment.ts
@@ -1,10 +1,10 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8081' // Ou l'URL de votre backend Spring Boot
-};
-
-// src/environments/environment.prod.ts
 // export const environment = {
-//   production: true,
-//   apiUrl: 'https://gestion-de-paie-backend.onrender.com' // <-- URL de votre Back-end sur Render
+//   production: false,
+//   apiUrl: 'http://localhost:8081' // Ou l'URL de votre backend Spring Boot
 // };
+
+//src/environments/environment.prod.ts
+export const environment = {
+  production: true,
+  apiUrl: 'https://gestion-de-paie-backend.onrender.com' // <-- URL de votre Back-end sur Render
+};
